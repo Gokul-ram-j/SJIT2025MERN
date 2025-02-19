6faa7870678557cbed91830aa4205c5ba20b1d23
Gokul-ram-j <gokulram221@gmail.com>
2025-02-19 14:33:36 +0530

    Hooks day9 fn

--- react/src/components/Home.jsx
+++ react/src/components/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Home({properties}) {
+  const [text,setText]=useState('')
   return (
     <div>
-      <h1>Home page</h1>
-      <h2>Below Tag is for props</h2>
-      <h3>Props:{properties.properties} {properties.sjit}</h3>
+      <h1>useState Example</h1>
+      <input type="text" onChange={(e)=>setText(e.target.value)} value={text} />
+      <h1>value typed {text}</h1>
     </div>
   )
 }
