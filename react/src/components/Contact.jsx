7ed0c734c4d2f7dafa967d1ab26d9212d9e7be84
Gokul-ram-j <gokulram221@gmail.com>
2025-02-19 13:19:03 +0530

    class component

--- react/src/components/Contact.jsx
+++ react/src/components/Contact.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function Contact() {
-  return (
-    <div>Contact</div>
-  )
+export default class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h1>it is a class component</h1>
+      </div>
+    )
+  }
 }
-
-export default Contact
