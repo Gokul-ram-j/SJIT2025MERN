c1c64fe3391d37f7ee672d4f600cb0626cae4dff
Gokul-ram-j <gokulram221@gmail.com>
2025-02-20 14:18:11 +0530

    day10 fn

--- react/src/components/Signup.jsx
+++ react/src/components/Signup.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 function Signup() {
+  const inputRef = useRef(null);
+
+  const handleFocus = () => {
+    inputRef.current.focus(); // Focuses the input field
+  };
+
   return (
-    <div>Signup</div>
-  )
+    <div>
+      <input ref={inputRef} type="text" placeholder="Type something..." />
+      <button onClick={handleFocus}>Focus Input</button>
+    </div>
+  );
 }
 
 export default Signup
