da114833794b26fbfd5bf90c58150086d3545cc0
Gokul-ram-j <gokulram221@gmail.com>
2025-02-20 08:30:57 +0530

    day8 task dropdowm

--- react/src/components/Home.jsx
+++ react/src/components/Home.jsx
@@ -1,17 +1,32 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-function Home({properties}) {
-  useEffect(()=>{
-    console.log('useEffect triggered when text change')
-  },[text])
-  const [text,setText]=useState('')
+function Home({ properties }) {
+  const [text, setText] = useState("");
+  useEffect(() => {
+    console.log("useEffect triggered when text change");
+  }, [text]);
+  const [showOptions, setShowOptions] = useState(false);
+
   return (
     <div>
       <h1>useState Example</h1>
-      <input type="text" onChange={(e)=>setText(e.target.value)} value={text} />
+      <input
+        type="text"
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+      />
       <h1>value typed {text}</h1>
+      <button onClick={()=>setShowOptions(!showOptions)}>
+        {showOptions ? "hide" : "show"}
+      </button>
+      {showOptions && (
+        <div>
+          <li>useState</li>
+          <li>useEffect</li>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
